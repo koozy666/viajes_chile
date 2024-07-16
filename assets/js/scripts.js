039bd8fee8bad663571eef3f8ee9025cf7019827
JAEN <yapur.1994@hotmail.com>
2024-07-16 15:18:00 -0400

    añadiendo comentarios

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    
+    // Inicializa el carrusel de Bootstrap con un intervalo de 2 segundos
     $('.carousel').carousel({
         interval: 2000 
     });
 
-    
+    // Añade desplazamiento suave a los enlaces de la barra de navegación
     document.querySelectorAll('.navbar-nav a').forEach(anchor => {
         anchor.addEventListener('click', function(e) {
             e.preventDefault();
@@ -13,17 +13,15 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         });
     });
-});
-document.addEventListener('DOMContentLoaded', function() {
+
+    // Maneja el envío del formulario de contacto
     const contactForm = document.getElementById('contactForm');
-
     contactForm.addEventListener('submit', function(event) {
-        event.preventDefault(); // Evitar la recarga de la página
-
-        // Mostrar un alert
+        event.preventDefault(); // Evita la recarga de la página
+        // Muestra un alert de confirmación
         alert('Gracias por enviar tu mensaje, te contactaremos a la brevedad.');
-
-        // Opcional: Limpiar el contenido del formulario
+        // Limpia el contenido del formulario
         contactForm.reset();
     });
 });
+
